f3145d6c19ab72c838ab8a862652e7ebe4817e59
MuradQ-88 <s2jbvv7sjs@privaterelay.appleid.com>
2026-06-02 08:47:42 +0200

    Added contact icon

--- components/MobileHero.tsx
+++ components/MobileHero.tsx
@@ -2,140 +2,80 @@ import Image from "next/image";
 
 export default function MobileHero() {
   return (
-    <div className="block lg:hidden relative w-full">
+    <div className="block lg:hidden">
 
-      {/* Background Logo */}
-      <div className="absolute inset-0 flex justify-center pointer-events-none">
-
-        <Image
-          src="/logo-new.png"
-          alt="Background Logo"
-          width={950}
-          height={950}
-          className="
-            opacity-15
-            object-contain
-            mt-20
-            scale-125
-          "
-        />
-
-      </div>
-
-      {/* Services */}
-      <div className="relative z-20 pt-16 px-4">
+      <div className="relative mx-auto h-[520px] w-full max-w-[430px]">
 
         {/* Row 1 */}
-        <div className="grid grid-cols-3 gap-4 text-center">
 
-          <ServiceIcon
-            icon="/icons/it-support.png"
-            title="IT SUPPORT"
+        <div className="absolute left-[10%] top-[170px]">
+          <Image
+            src="/icons/it-support.png"
+            alt="IT Support"
+            width={78}
+            height={78}
           />
+        </div>
 
-          <ServiceIcon
-            icon="/icons/hosting.png"
-            title="HOSTING"
+        <div className="absolute left-1/2 top-[170px] -translate-x-1/2">
+          <Image
+            src="/icons/hosting.png"
+            alt="Hosting"
+            width={78}
+            height={78}
           />
+        </div>
 
-          <ServiceIcon
-            icon="/icons/networking.png"
-            title="NETWORKING"
+        <div className="absolute right-[10%] top-[170px]">
+          <Image
+            src="/icons/networking.png"
+            alt="Networking"
+            width={78}
+            height={78}
           />
-
         </div>
 
         {/* Row 2 */}
-        <div className="grid grid-cols-3 gap-4 mt-10 text-center">
 
-          <ServiceIcon
-            icon="/icons/web-development.png"
-            title="WEB DEVELOPMENT"
+        <div className="absolute left-[10%] top-[290px]">
+          <Image
+            src="/icons/web-development.png"
+            alt="Web Development"
+            width={78}
+            height={78}
           />
+        </div>
 
-          <ServiceIcon
-            icon="/icons/cloud-based-solutions.png"
-            title="CLOUD SOLUTIONS"
+        <div className="absolute left-1/2 top-[290px] -translate-x-1/2">
+          <Image
+            src="/icons/cloud-based-solutions.png"
+            alt="Cloud Solutions"
+            width={78}
+            height={78}
           />
+        </div>
 
-          <ServiceIcon
-            icon="/icons/hardware-solutions.png"
-            title="HARDWARE"
+        <div className="absolute right-[10%] top-[290px]">
+          <Image
+            src="/icons/hardware-solutions.png"
+            alt="Hardware Solutions"
+            width={78}
+            height={78}
           />
-
         </div>
 
         {/* Contact */}
-        <div className="flex justify-center mt-12">
 
-          <a href="/contact">
-
-            <div className="flex flex-col items-center">
-
-              <Image
-                src="/icons/contact-box.png"
-                alt="Contact Us"
-                width={70}
-                height={70}
-                className="
-                  drop-shadow-[0_0_30px_rgba(168,85,247,0.8)]
-                "
-              />
-
-              <span
-                className="
-                  mt-2
-                  text-sm
-                  font-bold
-                  text-white
-                "
-              >
-                CONTACT US
-              </span>
-
-            </div>
-
-          </a>
-
+        <div className="absolute left-1/2 top-[415px] -translate-x-1/2">
+          <Image
+            src="/icons/contact-box.png"
+            alt="Contact"
+            width={52}
+            height={52}
+          />
         </div>
 
       </div>
-
-    </div>
-  );
-}
-
-function ServiceIcon({
-  icon,
-  title,
-}: {
-  icon: string;
-  title: string;
-}) {
-  return (
-    <div className="flex flex-col items-center">
-
-      <Image
-        src={icon}
-        alt={title}
-        width={110}
-        height={110}
-        className="
-          drop-shadow-[0_0_25px_rgba(34,211,238,0.5)]
-        "
-      />
-
-      <span
-        className="
-          mt-2
-          text-[11px]
-          font-bold
-          text-white
-          leading-tight
-        "
-      >
-        {title}
-      </span>
 
     </div>
   );
